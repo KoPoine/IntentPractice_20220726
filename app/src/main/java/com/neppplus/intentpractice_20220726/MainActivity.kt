ec8dc6906bb56bc7413e0e4aa81cbf9593e86afb
김민철 <kmc901206@gmail.com>
2022-07-26 16:21:51 +0900

    화면 전환시 데이터 첨부 Intent(2)

--- app/src/main/java/com/neppplus/intentpractice_20220726/MainActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_20220726/MainActivity.kt
@@ -14,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         moveBtn.setOnClickListener {
 //            티켓(myIntent)을 발권 - 출발지(this) -> 목적지(SecondActivity)로 이동하는 티켓
             val myIntent = Intent(this, SecondActivity::class.java)
+
+//            데이터(수화물) 첨부하는 코드 > 비행기 티켓(myIntent)에 뭔가(extra)를 첨부(put)한다.
+            val myName = dataEdt.text.toString()
+            myIntent.putExtra("myName", myName)
+
 //            비행기 출발
             startActivity(myIntent)
         }
--- app/src/main/java/com/neppplus/intentpractice_20220726/SecondActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_20220726/SecondActivity.kt
@@ -10,6 +10,12 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+//        MainActivty에서 보내준 데이터(수화물) 찾는 코드
+        val myName = intent.getStringExtra("myName")
+
+//        받아온 데이터(수화물) 활용 코드
+        dataTxt.text = myName
+
 //        다시 돌아가기
         backBtn.setOnClickListener {
 //            val myIntent = Intent(this, MainActivity::class.java)
