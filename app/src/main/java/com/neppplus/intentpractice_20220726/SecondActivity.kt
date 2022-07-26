f2540389365e7dd12425f4ddebb6718602920742
김민철 <kmc901206@gmail.com>
2022-07-26 15:48:44 +0900

    인텐트로 이동할 새로운 Activity를 생성

---
+++ app/src/main/java/com/neppplus/intentpractice_20220726/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intentpractice_20220726
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
