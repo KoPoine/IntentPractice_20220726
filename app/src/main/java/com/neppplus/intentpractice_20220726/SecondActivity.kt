b2cd1a2846c80bb7921a91411f39e57a4f7dde30
김민철 <kmc901206@gmail.com>
2022-07-26 16:08:00 +0900

    돌아가기 > finish() 활용

--- app/src/main/java/com/neppplus/intentpractice_20220726/SecondActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_20220726/SecondActivity.kt
@@ -1,11 +1,20 @@
 package com.neppplus.intentpractice_20220726
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        다시 돌아가기
+        backBtn.setOnClickListener {
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            finish()
+        }
     }
 }
